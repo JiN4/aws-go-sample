6c7a66b43b2c98a4fdcf32c09e2f3afeb14bbefa
尾崎仁一 <jin@jinsPC.local>
2018-12-21 03:48:16 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
-package main
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
-func main() {
-    r := gin.Default()
-    r.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Hello World")
-    })
-    r.Run() // listen and server on 0.0.0.0:8080
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"size:255"`
+	Password string `gorm:"size:255"`
+	Email    string `gorm:"size:255"`
 }
+
+db, err := gorm.Open("mysql", "root:kobedenshi@tcp(gs-group-weet-mysql.cnvtlozgscam.ap-northeast-1.rds.amazonaws.com:3306)/gs_group_weet?charset=utf8&parseTime=True&loc=Local")
+if err != nil {
+    panic("failed to connect database")
+}
+
+// 実行完了後DB接続を閉じる
+defer db.Close() 
+
+// ログ出力を有効にする
+db.LogMode(true) 
+
+sample.go
+// DBエンジンを「InnoDB」に設定
+db.Set("gorm:table_options", "ENGINE=InnoDB")
+
+// マイグレーション
+db.AutoMigrate(&User{})
