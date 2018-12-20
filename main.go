44a2cb5583376e49f8e9a8cd255af94d11531e99
尾崎仁一 <jin@jinsPC.local>
2018-12-21 04:03:53 +0900

    update

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+package main
+
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -10,20 +12,20 @@ type User struct {
 	Email    string `gorm:"size:255"`
 }
 
-db, err := gorm.Open("mysql", "root:kobedenshi@tcp(gs-group-weet-mysql.cnvtlozgscam.ap-northeast-1.rds.amazonaws.com:3306)/gs_group_weet?charset=utf8&parseTime=True&loc=Local")
-if err != nil {
-    panic("failed to connect database")
+func main() {
+	db, err := gorm.Open("mysql", "root:kobedenshi@tcp(gs-group-weet-mysql.cnvtlozgscam.ap-northeast-1.rds.amazonaws.com:3306)/gs_group_weet?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 実行完了後DB接続を閉じる
+	defer db.Close()
+
+	// ログ出力を有効にする
+	db.LogMode(true)
+	// DBエンジンを「InnoDB」に設定
+	db.Set("gorm:table_options", "ENGINE=InnoDB")
+
+	// マイグレーション
+	db.AutoMigrate(&User{})
 }
-
-// 実行完了後DB接続を閉じる
-defer db.Close() 
-
-// ログ出力を有効にする
-db.LogMode(true) 
-
-sample.go
-// DBエンジンを「InnoDB」に設定
-db.Set("gorm:table_options", "ENGINE=InnoDB")
-
-// マイグレーション
-db.AutoMigrate(&User{})
